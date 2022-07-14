7c072f2d34fda2905f4f96305a7d6899cd0ab396
Katie Johnston <KatieJRise1995@gmail.com>
2022-07-14 13:56:10 -0500

    Added Project

---
+++ katie-johnston-p2-main/README.md
@@ -0,0 +1,4 @@
+# katie-johnston-p2
+Repository for Katie Johnston - Project 2
+
+This project is a Covid-19 Tracker app that includes multiple charts displaying the various types of data both worldwide and within the US.
---
+++ katie-johnston-p2-main/covid-tracker-app/src/api/index.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
+
+  try {
+    const { data: { confirmed, deaths, lastUpdate } } = await axios.get(changeableUrl);
+
+    return { confirmed, deaths, lastUpdate };
+  } catch (error) {
+    return error;
+  }
+};
+
+// On start, display US data in Line Graph. When clicking another country, display bar graph
+export const fetchDailyData = async () => {
+    try {
+      const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
+      return data.map(({ positive, death, dateChecked: date }) => ({ confirmed: positive, deaths: death, date }));
+    } catch (error) {
+      return error;
+    }
+  };
+
+export const fetchCountries = async () => {
+  try {
+    const { data: { countries } } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
+  } catch (error) {
+    return error;
+  }
+};
